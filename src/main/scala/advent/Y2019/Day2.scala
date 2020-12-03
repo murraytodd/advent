11a2ec0a78b76e023c78d9b96aeea33a22bef0c0
Murray Todd Williams <murray.williams@accenture.com>
2020-12-03 11:33:13 -0600

    Cleaned Day 2

--- src/main/scala/advent/Y2019/Day2.scala
+++ src/main/scala/advent/Y2019/Day2.scala
@@ -1,16 +1,13 @@
 package advent.Y2019
 
 import scala.annotation.tailrec
-import scala.util.Using
 
 object Day2 {
 
   type State = Vector[Int]
 
   @tailrec
-  def compute(step: Int, state: State): State = {
-    // println(s"Computing step $step")
-    // println(s"State is $state")
+  def compute(state: State, step: Int = 0): State = {
     assert(step * 4 < state.size, "Program needed to terminate by now. Can't read next instructions.")
     val iPos = step * 4
     val xPos = iPos + 1
@@ -21,11 +18,10 @@ object Day2 {
     lazy val x = state(state(xPos))
     lazy val y = state(state(yPos))
     lazy val destination = state(resPos)
-    // if (instruction != 99) println(s"$instruction -> $x and $y to $destination")
 
-    state(iPos) match {
-      case 1 => compute(step + 1, state.updated(destination, x + y))
-      case 2 => compute(step + 1, state.updated(destination, x * y))
+    instruction match {
+      case 1 => compute(state.updated(destination, x + y), step + 1)
+      case 2 => compute(state.updated(destination, x * y), step + 1)
       case 99 => state
     }
   }
--- src/test/scala/advent/Y2019/TestDay2.scala
+++ src/test/scala/advent/Y2019/TestDay2.scala
@@ -9,15 +9,15 @@ class TestDay2 extends FunSuite with Matchers {
     .get.flatten.toVector
 
   test("Part 1 Samples") {
-    compute(0, Vector(1,0,0,0,99)) shouldBe Vector(2,0,0,0,99)
-    compute(0, Vector(2,3,0,3,99)) shouldBe Vector(2,3,0,6,99)
-    compute(0, Vector(2,4,4,5,99,0)) shouldBe Vector(2,4,4,5,99,9801)
-    compute(0, Vector(1,1,1,4,99,5,6,0,99)) shouldBe Vector(30,1,1,4,2,5,6,0,99)
+    compute(Vector(1,0,0,0,99)) shouldBe Vector(2,0,0,0,99)
+    compute(Vector(2,3,0,3,99)) shouldBe Vector(2,3,0,6,99)
+    compute(Vector(2,4,4,5,99,0)) shouldBe Vector(2,4,4,5,99,9801)
+    compute(Vector(1,1,1,4,99,5,6,0,99)) shouldBe Vector(30,1,1,4,2,5,6,0,99)
   }
 
   test("Part 1") {
     val fixed = program.updated(1, 12).updated(2, 2)
-    compute(0, fixed)(0) shouldBe 4576384
+    compute(fixed)(0) shouldBe 4576384
   }
 
   test("Part 2") {
@@ -25,7 +25,7 @@ class TestDay2 extends FunSuite with Matchers {
       noun <- 0 to 98
       verb <- 0 to 98
       fixed = program.updated(1,noun).updated(2,verb)
-      if compute(0, fixed)(0) == 19690720
+      if compute(fixed)(0) == 19690720
     } yield(noun * 100 + verb)
     inputs shouldBe Seq(5398)
   }
